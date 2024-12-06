c59ff3d331cfb00657f477c2a11b237aa0bb234f
anonymous <git@github.com>
2024-12-07 03:07:05 +0700

    remove unused

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 use Laravel\Sanctum\NewAccessToken;
 
